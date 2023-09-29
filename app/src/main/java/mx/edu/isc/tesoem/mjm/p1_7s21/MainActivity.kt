b1d6d2e89706075f6a27c2281ef1d1aab2ebf11e
Moises Mendez J <moikita_zmen156@hotmail.com>
2023-09-29 11:48:03 -0600

    mjm

---
+++ app/src/main/java/mx/edu/isc/tesoem/mjm/p1_7s21/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.isc.tesoem.mjm.p1_7s21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.isc.tesoem.mjm.p1_7s21.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
